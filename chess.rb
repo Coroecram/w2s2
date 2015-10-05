8028c6c901b8b62fc551dd803b6823695fe3523e
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-05 12:01:07 -0400

    Add render and colorize gem.

--- chess.rb
+++ chess.rb
@@ -1,12 +1,21 @@
+require 'colorize'
+
 class ChessPiece
+  attr_reader :position
 
   def initialize(board, position)
     @position = position
     @board = board
   end
+
+  def to_s
+    "8 "
+  end
+
 end
 
 class Board
+  attr_reader :grid
 
   def initialize
     @grid = Array.new(8) { |row| Array.new(8) { |col| ChessPiece.new(self, [row, col]) } }
@@ -15,4 +24,23 @@ class Board
 end
 
 class Display
+
+  def initialize(board)
+    @board = board
+    @cursor_position = [0, 0]
+  end
+
+  def render
+    @board.grid.each do |row|
+      row.each do |col|
+        if col.position == cursor.position
+          print col.colorize(:red)
+        end
+        print col
+      end
+      puts
+    end
+  end
+
+
 end
