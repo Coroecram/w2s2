d49cb90d21b1e48b7631b399111c2f1175b85afe
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-06 17:14:00 -0400

    Add dynamic and static board

--- chess/chess_GUI.rb
+++ chess/chess_GUI.rb
@@ -5,21 +5,24 @@ load './chess_board.rb'
 
 class Display
   attr_reader :board
-  attr_accessor :cursor_position, :selected_position, :current_team
+  attr_accessor :cursor_position, :selected_position, :current_team, :flipped
 
   def initialize(board)
     @selected_position = nil
     @board = board
     @cursor_position = [5, 4]
     @current_team = :white
+    @flipped = false
   end
 
   def render
     background_color = nil
-    @board.grid.each do |row|
-      # print " ".colorize(:background => background_color)
+    flipped == true ? render_grid = @board.grid.reverse : render_grid = @board.grid
+    render_grid.each do |row|
       background_color = (background_color.nil? ? :light_black : nil)
-      row.each do |col|
+
+      flipped == true ? render_row = row.reverse : render_row = row
+      render_row.each do |col|
         background_color = (background_color.nil? ? :light_black : nil)
         if col.position == cursor_position
 
@@ -70,6 +73,9 @@ class Display
     draw_board
     board.flip
     current_team == :white ? self.current_team = :black : self.current_team = :white
+    unless flipped.nil?
+      flipped == false ? self.flipped = true : self.flipped = false
+    end
   end
 
   def other_team
@@ -112,13 +118,13 @@ class Display
     when "\e"
       abort
     when "\e[A"
-      move_cursor(:up)
+      flipped ? move_cursor(:down) : move_cursor(:up)
     when "\e[B"
-      move_cursor(:down)
+      flipped ? move_cursor(:up) : move_cursor(:down)
     when "\e[C"
-      move_cursor(:right)
+      flipped ? move_cursor(:left) : move_cursor(:right)
     when "\e[D"
-      move_cursor(:left)
+      flipped ? move_cursor(:right) : move_cursor(:left)
     end
   end
 
